8b2187f428984fd3feca50199621a7277bf37ada
_Txsla <104021913+Tesla6426@users.noreply.github.com>
2024-12-04 10:20:39 -0800

    removed bad code
    the prev spam code was going to be inefficient - went with a system similar to how NBT Archves items are sorted instead

--- src/main/java/net/txsla/proxychat/spamLimiter.java
+++ src/main/java/net/txsla/proxychat/spamLimiter.java
@@ -1,16 +1,54 @@
 package net.txsla.proxychat;
 
 import com.velocitypowered.api.proxy.Player;
+import org.checkerframework.checker.units.qual.A;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class spamLimiter {
-    // limit spam
-    public static List<Spam> players = new ArrayList<>() {
-    };
+    static List<Player> index = new ArrayList<>();
+    static List<Integer> counter = new ArrayList<>();
+    static int maxCounter;
+    static int decTimer;
+
     public static void addPlayer(Player player) {
-        // no dupe protection
-        players.add(new Spam(player));
+        // do NOT add if player is already added to counter/index
+        if (index.contains(player)) return;
+        // set info
+        index.add(player);
+        counter.add(0);
+    }
+    public static void inc(Player player, int i) {
+        // set to a negative number to subtract
+        // simplify down to 1 line later (no need to store an int)
+        int x = index.indexOf(player);
+        counter.set(x, counter.get(x) + i);
+    }
+    public boolean canChat(Player player) {
+        // simplify down to 1 line later (no need to store an int)
+        int x = index.indexOf(player);
+        return counter.get(x) <= maxCounter;
+    }
+    public static void startDecTimer() {
+        Thread dec = new Thread(()->
+                {
+                    int i =0;
+                    while (true) {
+                        i = 0;
+                        // wait
+                        try { Thread.sleep(decTimer); } catch (InterruptedException e) {
+                            throw new RuntimeException(e);}
+
+                        // decrement one from everyone's chat timer
+                        for (int count : counter) {
+                            counter.set(i, count-1);
+                            i++;
+                        }
+
+                    }
+                }
+                );
+        dec.start();
     }
 }
